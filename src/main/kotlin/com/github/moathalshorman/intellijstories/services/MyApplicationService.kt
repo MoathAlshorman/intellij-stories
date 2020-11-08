5f2e811541838560f688fee66dd27a55546f3885
GitHub Action <action@github.com>
2020-11-08 17:00:56 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/moathalshorman/intellijstories/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.moathalshorman.intellijstories.services
+
+import com.github.moathalshorman.intellijstories.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/moathalshorman/intellijstories/services/MyProjectService.kt
+++ src/main/kotlin/com/github/moathalshorman/intellijstories/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.moathalshorman.intellijstories.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.moathalshorman.intellijstories.MyBundle
 
 class MyProjectService(project: Project) {
 
